5083161879bd92656e7fe1fb0db108c3abfa4cdf
Jyotirmoy Paul <cipherresfeber@gmail.com>
2020-06-22 11:10:12 +0530

    movi api - anime detail view implemented

--- movi-api/app.js
+++ movi-api/app.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const https = require("https");
 const { parse } = require("node-html-parser");
-const { json } = require("express");
 
 const app = express();
 
-const _baseQueryUrl = "https://www.kickassanime.rs/search?q=";
+const _baseUrl = "https://www.kickassanime.rs/";
 const _error = "ERROR";
+
+function getJsonError(errorMessage) {
+  return {
+    status: _error,
+    message: errorMessage,
+  };
+}
 
 function getRawJson(responseBody) {
   var root = parse(responseBody, {
@@ -23,31 +29,52 @@ function getRawJson(responseBody) {
     .split(" appData = ")[1]
     .split(" || {}")[0];
 
-  return jsonResponse;
+  return JSON.parse(jsonResponse);
 }
 
-app.get("/api/query=:query", async (req, res) => {
-  const url = _baseQueryUrl + req.params.query;
+// base query api - for querying about for an anime name
+app.get("/api/query=:query", (req, res) => {
+  const url = `${_baseUrl}search?q=${req.params.query}`;
 
   https
     .get(url, (response) => {
       var body = [];
 
-      response.on("data", (chunk) => {
-        body.push(chunk);
-      });
+      response.on("data", (chunk) => body.push(chunk));
 
       response.on("end", () => {
         body = Buffer.concat(body).toString();
 
         const jsonResponse = getRawJson(body);
 
-        res.send(jsonResponse);
+        res.send(jsonResponse.animes);
       });
     })
     .on("error", (err) => {
-      console.log(err);
-      res.send(_error);
+      res.send(getJsonError(err.toString()));
+    });
+});
+
+// get details on a particular anime
+app.get("/api/anime/:animeName", (req, res) => {
+  const url = `${_baseUrl}/anime/${req.params.animeName}`;
+
+  https
+    .get(url, (response) => {
+      var body = [];
+
+      response.on("data", (chunk) => body.push(chunk));
+
+      response.on("end", () => {
+        body = Buffer.concat(body).toString();
+
+        const jsonResponse = getRawJson(body);
+
+        res.send(jsonResponse.anime);
+      });
+    })
+    .on("error", (err) => {
+      res.send(getJsonError(err.toString()));
     });
 });
 
